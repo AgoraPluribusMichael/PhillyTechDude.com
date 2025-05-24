56f3bf5a29fe83682674638ba6a1f3033e97c531
msbmteam <phillytechdudebro69@gmail.com>
2025-05-24 12:20:41 -0400

    Use _redirects

--- scripts/on-load.js
+++ scripts/on-load.js
@@ -1,42 +1,36 @@
 $(document).ready(function() {
-    var href = window.location.href;
-    if (!href.startsWith("file://") && href.toLowerCase().includes("phillytechdudebro")) {
-        window.location.href = "https://phillytechdude.com";  // Redirect to new URL
-    } else {
-        $("head").prepend(
-        '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />'
-        );
-        $("head").append(
-            '<!-- Google tag (gtag.js) -->'+
-            '<script async src="https://www.googletagmanager.com/gtag/js?id=G-TR4TKL038J"></script>'+
-            '<script>'+
-            '  window.dataLayer = window.dataLayer || [];'+
-            '  function gtag(){dataLayer.push(arguments);}'+
-            "  gtag('js', new Date());"+
-            "  gtag('config', 'G-TR4TKL038J');"+
-            '</script>'
-        );
-        $("body").prepend(
-            '<div id="topbanner" style="background-image: url(images/banner.jpg);"></div>'+
-            '<nav id="topbar" data-spy="affix" data-offset-top="500">'+
-                '<ul id="nav">'+
-                    '<a href="index.html"><li id="homeTopbar" class="menuButton">✊Rise Up</li></a>'+
-                    '<a href="democracy.html"><li id="democracyTopbar" class="menuButton">🗽Democracy</li></a>'+
-                    '<a href="economy.html"><li id="economyTopbar" class="menuButton">💸Economy</li></a>'+
-                    '<a href="taxes.html"><li id="taxesTopbar" class="menuButton">🔰Taxes</li></a>'+
-                    '<a href="social_security.html"><li id="socialSecurityTopbar" class="menuButton">👴Social Security</li></a>'+
-                    '<a href="policies.html"><li id="policiesTopbar" class="menuButton">⚖️Other Policies</li></a>'+
-                '</ul>'+
-            '</nav>'
-        );
-        $("body").append(
-            '<div class="text panel panel-default">'+
-                '<footer>'+
-                    '<p><img src="PhillyTechDude.png"></img></p>'+
-                    '<p>I\'m a 28-year-old software engineer living in South Philly who is considering running for office in 2026 to represent PA-03 in Congress, because I\'m tired of writing code designed to make the rich people richer by harming our society, and I would instead prefer to advocate for and write laws to help serve the common good.</p>'+
-                '</footer>'+
-            '</div>'
-        );
-    }
-	
+    $("head").prepend(
+    '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />'
+    );
+    $("head").append(
+        '<!-- Google tag (gtag.js) -->'+
+        '<script async src="https://www.googletagmanager.com/gtag/js?id=G-TR4TKL038J"></script>'+
+        '<script>'+
+        '  window.dataLayer = window.dataLayer || [];'+
+        '  function gtag(){dataLayer.push(arguments);}'+
+        "  gtag('js', new Date());"+
+        "  gtag('config', 'G-TR4TKL038J');"+
+        '</script>'
+    );
+    $("body").prepend(
+        '<div id="topbanner" style="background-image: url(images/banner.jpg);"></div>'+
+        '<nav id="topbar" data-spy="affix" data-offset-top="500">'+
+            '<ul id="nav">'+
+                '<a href="index.html"><li id="homeTopbar" class="menuButton">✊Rise Up</li></a>'+
+                '<a href="democracy.html"><li id="democracyTopbar" class="menuButton">🗽Democracy</li></a>'+
+                '<a href="economy.html"><li id="economyTopbar" class="menuButton">💸Economy</li></a>'+
+                '<a href="taxes.html"><li id="taxesTopbar" class="menuButton">🔰Taxes</li></a>'+
+                '<a href="social_security.html"><li id="socialSecurityTopbar" class="menuButton">👴Social Security</li></a>'+
+                '<a href="policies.html"><li id="policiesTopbar" class="menuButton">⚖️Other Policies</li></a>'+
+            '</ul>'+
+        '</nav>'
+    );
+    $("body").append(
+        '<div class="text panel panel-default">'+
+            '<footer>'+
+                '<p><img src="PhillyTechDude.png"></img></p>'+
+                '<p>I\'m a 28-year-old software engineer living in South Philly who is considering running for office in 2026 to represent PA-03 in Congress, because I\'m tired of writing code designed to make the rich people richer by harming our society, and I would instead prefer to advocate for and write laws to help serve the common good.</p>'+
+            '</footer>'+
+        '</div>'
+    );
 });
