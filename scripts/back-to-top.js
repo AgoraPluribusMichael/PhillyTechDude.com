1a6f1f144919f804dfcbffc8affb69ba90774146
msbmteam <phillytechdudebro69@gmail.com>
2025-05-31 17:43:48 -0400

    Back to top button tweaks

--- scripts/back-to-top.js
+++ scripts/back-to-top.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
     var backToTopButton = document.createElement("button");
-    backToTopButton.id = "backToTop";
+    backToTopButton.id = "back-to-top";
     backToTopButton.title = "Back to Top";
     backToTopButton.innerText = "Back to Top";
     $("body").append(backToTopButton);
 
     // Scroll to top when clicked
-    document.getElementById("backToTop").addEventListener("click", function() {
+    document.getElementById("back-to-top").addEventListener("click", function() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
-    // Show button after scrolling down 800px
     window.onscroll = function() {
-        const btn = document.getElementById("backToTop");
-        btn.style.display = window.scrollY > 800 ? "block" : "none";
+        showButton();
     };
+    showButton();
 });
+
+// Show button after scrolling down 800px
+function showButton() {
+    const btn = document.getElementById("back-to-top");
+    btn.style.display = window.scrollY > 800 ? "block" : "none";
+}
